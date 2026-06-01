3d04e7f770a5526cae00fecd17084353d0218363
yodoshia <136631690+yodoshia@users.noreply.github.com>
2026-06-01 18:57:05 +0000

    v1

--- objeto.js
+++ objeto.js
@@ -1,1 +1,20 @@
+const animal ={
+nome: "mendingo",
+especie: "cachorro",// string
+idade: 8, // number
+vacinado: false,//boolean
+tutor: false
+};
 
+console.log(animal.nome);
+console.log(animal.especie);
+//console.log(animal.idade);
+
+animal.idade = "7 anos"
+animal.nome = "sapao"
+animal.vacinado = true
+console.log(animal.idade)
+console.log(animal);
+
+console.log(`O ${animal.especie} chama-se ${animal.nome}!`);
+console.log(`O ${animal.nome} tem ${animal.idade}!`);
